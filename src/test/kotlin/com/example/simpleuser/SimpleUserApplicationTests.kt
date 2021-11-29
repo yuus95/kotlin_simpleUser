a06187634df9588801ec5bcf7dad2a431e736603
yushin <kkad45@naver.com>
2021-11-29 14:43:53 +0900

    [add] 초기화

---
+++ src/test/kotlin/com/example/simpleuser/SimpleUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.simpleuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleUserApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
